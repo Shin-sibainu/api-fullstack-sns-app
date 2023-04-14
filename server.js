5d5bf6cbb0a9373529e3adf5e470e62ce2b6af94
Shin-sibainu <shincode0712@gmail.com>
2023-04-14 21:21:29 +0900

    mod

--- server.js
+++ server.js
@@ -5,12 +5,26 @@ const userRoute = require("./routes/users");
 const authRoute = require("./routes/auth");
 const postRoute = require("./routes/posts");
 const cors = require("cors");
-const serverless = require("serverless-http");
+const serverlessExpress = require("@vendia/serverless-express");
+const PORT = 5000;
+
+if (process.env.NODE_ENV !== "production") {
+  app.listen(PORT, () => {
+    console.log("server is running");
+  });
+} else {
+  const server = serverlessExpress.createServer(app);
+  exports.handler = (event, context) =>
+    serverlessExpress.proxy(server, event, context);
+}
+
+// ローカル確認用
+// app.listen(PORT);
+// console.log("Loading server.js...");
+// exports.handler = serverlessExpress({ app });
 
 app.use(cors());
 app.use(express.json());
 app.use("/api/auth", authRoute);
 app.use("/api/posts", postRoute);
 app.use("/api/users", userRoute);
-
-module.exports.handler = serverless(app);
