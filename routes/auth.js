2901402caa5a43196b95caa6a265f22529ba111b
Shin-sibainu <shincode0712@gmail.com>
2023-04-16 14:17:28 +0900

    mod

--- routes/auth.js
+++ routes/auth.js
@@ -30,6 +30,8 @@ router.post("/register", async (req, res) => {
     },
   });
 
+  // await prisma.$disconnect();
+
   return res.json({ user });
 });
 
--- server.js
+++ server.js
@@ -1,31 +1,40 @@
 const express = require("express");
 const app = express();
-const dotenv = require("dotenv").config();
+require("dotenv").config();
 const userRoute = require("./routes/users");
 const authRoute = require("./routes/auth");
 const postRoute = require("./routes/posts");
 const cors = require("cors");
-const serverlessExpress = require("@vendia/serverless-express");
+// const serverlessExpress = require("@vendia/serverless-express");
+const { PrismaClient } = require("@prisma/client");
+const prisma = new PrismaClient();
+// const awsServerlessExpress = require("aws-serverless-express");
+// const server = awsServerlessExpress.createServer(app);
+const serverless = require("serverless-http");
 const PORT = 5000;
-
-if (process.env.NODE_ENV !== "production") {
-  console.log(process.env.NODE_ENV);
-  app.listen(PORT, () => {
-    console.log("server is running");
-  });
-} else {
-  const server = serverlessExpress.createServer(app);
-  exports.handler = (event, context) =>
-    serverlessExpress.proxy(server, event, context);
-}
 
 // ローカル確認用
 // app.listen(PORT);
 // console.log("Loading server.js...");
 // exports.handler = serverlessExpress({ app });
 
+//https://nkgr.hatenablog.com/entry/2018/07/01/210000
+//prismaをserverlessデプロイ
+//https://kiririmode.hatenablog.jp/entry/20220619/1655622443
+
 app.use(cors());
 app.use(express.json());
 app.use("/api/auth", authRoute);
 app.use("/api/posts", postRoute);
 app.use("/api/users", userRoute);
+
+if (process.env.NODE_ENV !== "production") {
+  app.listen(PORT, () => {
+    console.log("server is running");
+  });
+} else {
+  // exports.handler = (event, context) => {
+  //   awsServerlessExpress.proxy(server, event, context);
+  // };
+  module.exports.handler = serverless(app);
+}
