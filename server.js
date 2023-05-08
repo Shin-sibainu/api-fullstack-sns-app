fd946052af4c2c3aa2838489f6cd1951e7a4532a
Shin-sibainu <shincode0712@gmail.com>
2023-05-08 15:26:34 +0900

    done

--- server.js
+++ server.js
@@ -6,8 +6,6 @@ const authRoute = require("./routes/auth");
 const postRoute = require("./routes/posts");
 const cors = require("cors");
 // const serverlessExpress = require("@vendia/serverless-express");
-const { PrismaClient } = require("@prisma/client");
-const prisma = new PrismaClient();
 // const awsServerlessExpress = require("aws-serverless-express");
 // const server = awsServerlessExpress.createServer(app);
 const serverless = require("serverless-http");
@@ -21,6 +19,8 @@ const PORT = 5000;
 //https://nkgr.hatenablog.com/entry/2018/07/01/210000
 //prismaをserverlessデプロイ
 //https://kiririmode.hatenablog.jp/entry/20220619/1655622443
+//prisma公式
+//https://www.prisma.io/docs/guides/deployment/deployment-guides/deploying-to-aws-lambda
 
 app.use(cors());
 app.use(express.json());
